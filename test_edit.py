0c04cd992eba95bdf9e6e2a8a6c5b36306fd8b3e
MyAccDhika <apriandhika@gmail.com>
2025-03-09 17:28:35 +0700

    'Fixed different data length'

--- test_edit.py
+++ test_edit.py
@@ -5,12 +5,12 @@ filepath = "C:/Users/apria/Project/Python/TitanicML_Competition/Titanic_Dataset/
 data = pd.read_csv(filepath)
 
 data['Age'] = data['Age'].fillna(round(data['Age'].mean()))
+data['Fare'] = data['Fare'].fillna(round(data['Fare'].mean()))
 
 data = data.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
 data['Sex'] = np.where(data['Sex'] == 'male', 1, 0)
 
-data = data.dropna()
 data['Embarked'] = data['Embarked'].astype('category').cat.codes
 
-#data.to_csv('C:/Users/apria/Project/Python/TitanicML_Competition/Titanic_Dataset/adv_test.csv')
+data.to_csv('C:/Users/apria/Project/Python/TitanicML_Competition/Titanic_Dataset/adv_test.csv')
